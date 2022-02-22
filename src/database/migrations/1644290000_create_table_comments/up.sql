573c2c9d6efdf19880d39363976ccb91f26f35c6
rosaliebegin <rosaliebegin@yahoo.com>
2022-02-22 17:18:40 -0500

    Update up.sql

--- src/database/migrations/1644290000_create_table_comments/up.sql
+++ src/database/migrations/1644290000_create_table_comments/up.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_post` int NOT NULL,
+  `post_id` int NOT NULL,
+  `body` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT NOW(),
   `updated_at` datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`, `post_id`) REFERENCES users(`id`),
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
