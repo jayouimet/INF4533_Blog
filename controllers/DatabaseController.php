88b55ce846694199ed7e593e7088d1d2c4486c97
jayouimet <jayouimet@hotmail.com>
2022-02-22 16:52:07 -0500

    Migrate routes

---
+++ controllers/DatabaseController.php
@@ -0,0 +1,43 @@
+<?php
+    require_once dirname(__FILE__) . '/../src/Request.php';
+    require_once dirname(__FILE__) . '/../src/Response.php';
+
+    class DatabaseController extends Controller {
+        /**
+         * Function called when trying to use the method Get on the home page
+         *
+         * @param Request $request
+         * @param Response $response
+         * @return void
+         */
+        public function getUp(Request $request, Response $response) {
+            require_once dirname(__FILE__) . '/../scripts/migrate_up.php';
+
+            $params = [
+                'name' => "Migrated up"
+            ];
+
+            // $_SESSION["test"] = "testSession";
+            /*if (isset($_SESSION['test'])) {
+                unset($_SESSION['test']);
+            }*/
+
+            return $this->render('home', $params);
+        }
+
+        public function getDown(Request $request, Response $response) {
+            require_once dirname(__FILE__) . '/../scripts/migrate_down.php';
+
+            $params = [
+                'name' => "Migrated down"
+            ];
+
+            // $_SESSION["test"] = "testSession";
+            /*if (isset($_SESSION['test'])) {
+                unset($_SESSION['test']);
+            }*/
+
+            return $this->render('home', $params);
+        }
+    }
+?>
