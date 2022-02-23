0e9bafe2a006e15ab233128c4a87baa1b0d4c0f9
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2022-02-23 17:37:53 -0500

    Changed the addPost

--- views/posts/addPost.php
+++ views/posts/addPost.php
@@ -1,6 +1,7 @@
 <form action="" method="POST">
-    <input placeholder="user id" name="user_id" type="text">
-    <input placeholder="title" name="title" type="text">
-    <input placeholder="image" name="post_image" type="file">
-    <textarea name="body" placeholder="Body of the post">
+    <label for="user_id">Author ID</label>      <input name="user_id" type="text"/>
+    <label for="title">Title</label>            <input placeholder="title" name="title" type="text"/>
+    <label for="post_image">Image</label>       <input placeholder="image" name="post_image" type="file"/>
+    <label for="body">Body</label>              <textarea name="body" placeholder="Body of the post"></textarea>
+    <input type="submit" value="Submit">
 </form>
