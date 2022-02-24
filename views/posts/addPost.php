69bb4dd5661e8590476fa718a873d4ec829c71fd
rosaliebegin <rosaliebegin@yahoo.com>
2022-02-23 19:11:33 -0500

    required pour addpost

--- views/posts/addPost.php
+++ views/posts/addPost.php
@@ -1,7 +1,7 @@
 <form action="" method="POST">
     <label for="user_id">Author ID</label>      <input name="user_id" type="text"/>
-    <label for="title">Title</label>            <input placeholder="title" name="title" type="text"/>
-    <label for="post_image">Image</label>       <input placeholder="image" name="post_image" type="file"/>
-    <label for="body">Body</label>              <textarea name="body" placeholder="Body of the post"></textarea>
+    <label for="title">Title</label>            <input placeholder="title" name="title" type="text" required minlength='1'/>
+    <label for="post_image">Image</label>       <input placeholder="image" name="post_image" type="file" required minlength='1'/>
+    <label for="body">Body</label>              <textarea name="body" placeholder="Body of the post" required minlength='1'></textarea>
     <input type="submit" value="Submit">
 </form>
