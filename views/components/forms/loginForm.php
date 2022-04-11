e6e282e798c57332b86b31059eb0084839d10882
jayouimet <jayouimet@hotmail.com>
2022-04-11 19:08:47 -0400

    background on login

--- views/components/forms/loginForm.php
+++ views/components/forms/loginForm.php
@@ -1,20 +1,26 @@
 <?php
 require_once dirname(__FILE__) . '/../../../src/Application.php';
-
-echo "<form action='". Application::$baseUrl ."/login' method='post'>";
-echo "<label for='username'>Username : </label><input type='text' name='username' required><br>" ;
-echo "<label for='password'>Password : </label><input type='password' name='password' minlength='8' required><br>" ;
-echo "<input label='login' type='submit'>";
-echo "</form>";
-
-if (isset($errorMessageId)) {
-    switch ($errorMessageId) {
-        case "invalidCredentials":
-            echo "Invalid username/password combination.";
-            break;
-        default:
-            echo "An unexpected error has occured, please try again later.";
-            break;
-    }
-}
 ?>
+<div class="login-form-container">
+    <?php
+    echo "<form action='". Application::$baseUrl ."/login' method='post'>";
+    if (isset($errorMessageId)) {
+        echo "<div>";
+        switch ($errorMessageId) {
+            case "invalidCredentials":
+                echo "Invalid username/password combination.";
+                break;
+            default:
+                echo "An unexpected error has occured, please try again later.";
+                break;
+        }
+        echo "</div>";
+    }
+    echo "<label for='username'>Username : </label><input type='text' name='username' required><br>" ;
+    echo "<label for='password'>Password : </label><input type='password' name='password' minlength='8' required><br>" ;
+    echo "<input label='login' type='submit'>";
+    echo "</form>";
+
+    echo "<a href='". Application::$baseUrl ."/register'>Click here to create an account.</a>";
+    ?>
+</div>
--- views/pages/users/login/content.php
+++ views/pages/users/login/content.php
@@ -1,8 +1,16 @@
 <?php
 require_once dirname(__FILE__) . '/../../../../src/Application.php';
 require dirname(__FILE__) . '/../../../components/common/navbar.php';
-
-require dirname(__FILE__) . '/../../../components/forms/loginForm.php';
-
-echo "<a href='". Application::$baseUrl ."/register'>Click here to create an account.</a>";
 ?>
+
+<div class="login-background">
+    <div class="background-img">
+    </div>
+</div>
+<div class="content-container">
+    <div class="">
+        <?php
+        require dirname(__FILE__) . '/../../../components/forms/loginForm.php';
+        ?>
+    </div>
+</div>
