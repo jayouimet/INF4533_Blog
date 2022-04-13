a7152a3e5f280db7f27558c183c3f2423122425b
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2022-04-13 14:28:51 -0400

    Update postList.php

--- views/components/lists/postList.php
+++ views/components/lists/postList.php
@@ -1,6 +1,24 @@
-<?php require_once dirname(__FILE__) . '/../../../src/Application.php'; ?>
+<?php require_once dirname(__FILE__) . '/../../../src/Application.php'; 
+function convertDate($now, $date){
+    $diff = $now - strtotime($date);
+    $d = date("s:i:H:d:m:Y", $diff);
+    $placeholders = ["second", "minute", "hour", "day", "month", "year"];
+    $arr = explode(":", $d);
+    $rtn = "";
+    $arr[5] -= 1970;
+
+    for($i = 0; $i < count($arr); $i++){
+        if($arr[$i] > 0) {
+            $rtn = $arr[$i] . " " . $placeholders[$i] . ($arr[$i] > 1 ? "s" : "");
+        }
+    }
+
+    return ltrim($rtn,"0");
+}
+?>
 <div id="posts">
     <?php
+    $now = time();
     foreach ($posts as $post) {
         $post->fetch();
 
@@ -12,7 +30,8 @@
         <div class="col-12" style="margin:0.4vmax;">
             <!-- Posted by date -->
             <div class="row">
-                <div class="author">Posted by <b><?php echo $post->user->username . "</b> at " . $post->created_at ?></div>
+                <div class="author">Posted by <b><?php echo $post->user->username ?></b>
+                <?php echo convertDate($now, $post->created_at); ?> ago</div>
             </div>
             <!-- Title -->
             <div class="row">
