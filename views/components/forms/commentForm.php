42263105516152ba4c52f3340c663638128c6236
jayouimet <jayouimet@hotmail.com>
2022-04-20 23:42:05 -0400

    Update commentForm.php

--- views/components/forms/commentForm.php
+++ views/components/forms/commentForm.php
@@ -3,7 +3,7 @@ require_once dirname(__FILE__) . '/../../../src/Application.php';
 require_once dirname(__FILE__) . '/../../../src/providers/AuthProvider.php';
 ?>
 
-<form action='". Application::$baseUrl . "/addcomment/" . $post_id . "' method='post'>
+<form action="<?php echo Application::$baseUrl . "/addcomment/" . $post_id ?>" method='post'>
     Comment : <input type='text' name='comment' required minlength='1' <?php echo (AuthProvider::isAuthed() ? null : "disabled"); ?>>
     <input <?php echo (AuthProvider::isAuthed() ? null : "disabled"); ?> type='submit'>
 </form>
